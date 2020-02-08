5f91f045830d134ab7f207bd6fe2c89bc6872624
Kavin Phan <kphan@Kavins-MacBook-Pro.local>
2020-02-08 15:33:34 -0500

    Fix

--- app/py/main.py
+++ app/py/main.py
@@ -2,11 +2,10 @@ import pose_analyze as pa
 import time
 import sys
 
-while True:
-    send("hello world")
-    time.sleep(1)
-
-
 def send(message):
     print(message)
     sys.stdout.flush()
+
+while True:
+    send("hello world")
+    time.sleep(1)
