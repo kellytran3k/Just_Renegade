2d1d02c457799d5cab938470d53d24a4ab3d3c85
Sarah Yoo <gayeon1505@gmail.com>
2020-02-08 13:27:12 -0500

    delete openDevTools

--- main.js
+++ main.js
@@ -13,9 +13,6 @@ function createWindow() {
 
   // and load the index.html of the app.
   win.loadFile("index.html");
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
